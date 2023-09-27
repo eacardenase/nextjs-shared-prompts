272697adebc03ef2830db2b564fa25721c7ac2e5
Edwin Cardenas <eacardenase@unal.edu.co>
2023-09-27 15:26:55 -0500

    feat: project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
