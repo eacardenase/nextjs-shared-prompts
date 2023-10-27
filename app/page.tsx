8ff16f9d0c72ec59392c168bbc828113beffd073
Edwin Cardenas <eacardenase@unal.edu.co>
2023-10-27 18:22:06 -0500

    feat: desktop navigation

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ const Home = () => {
     <section className='flex w-full flex-col items-center justify-center'>
       <h1 className='mt-5 text-center text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl'>
         Discover & Share <br className='max-md:hidden' />
+        {/** max-md hidden for smaller devices */}
         <span className='bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-center text-transparent'>
           AI-Powered Prompts
         </span>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,10 +4,33 @@ import { useState, useEffect } from 'react';
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import {
+  signIn,
+  signOut,
+  useSession,
+  getProviders,
+  LiteralUnion,
+  ClientSafeProvider,
+} from 'next-auth/react';
+import { BuiltInProviderType } from 'next-auth/providers/index';
 
 const Nav = () => {
   const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState<Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null>(null);
+
+  useEffect(() => {
+    const setupProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+
+    setupProviders();
+  }, []);
+
   return (
     <nav className='mb-16 flex w-full items-center justify-between pt-3'>
       <Link href='/' className='flex items-center justify-center gap-2'>
@@ -41,9 +64,31 @@ const Nav = () => {
             >
               Sign Out
             </button>
+
+            <Link href='/profile'>
+              <Image
+                src='assets/images/logo.svg'
+                alt='Profile Picture'
+                width={37}
+                height={37}
+                className='rounded-full'
+              />
+            </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='flex items-center justify-center rounded-full border border-black bg-black px-5 py-1.5 text-center font-inter text-sm text-white transition-all hover:bg-white hover:text-white'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
