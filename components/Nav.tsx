8c27f4cfacd800e6684b147bf4a7b7d0f74eb6cc
Edwin Cardenas <eacardenase@unal.edu.co>
2023-10-28 11:43:24 -0500

    feat: mobile navigation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -21,6 +21,7 @@ const Nav = () => {
     LiteralUnion<BuiltInProviderType, string>,
     ClientSafeProvider
   > | null>(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setupProviders = async () => {
@@ -74,6 +75,66 @@ const Nav = () => {
                 className='rounded-full'
               />
             </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='flex items-center justify-center rounded-full border border-black bg-black px-5 py-1.5 text-center font-inter text-sm text-white transition-all hover:bg-white hover:text-white'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className='relative flex sm:hidden'>
+        {/** sm:hidden -> hidden for larger devices */}
+        {isUserLoggedIn ? (
+          <div className='flex'>
+            <Image
+              src='assets/images/logo.svg'
+              alt='Profile Picture'
+              width={37}
+              height={37}
+              className='rounded-full'
+              onClick={() => setToggleDropdown((prevState) => !prevState)} // it's a good practice to not update the state by itself
+            />
+
+            {toggleDropdown && (
+              <div className='absolute right-0 top-full mt-3 flex w-full min-w-[210px] flex-col items-end justify-end gap-2 rounded-lg bg-white p-5'>
+                <Link
+                  href='/profile'
+                  className='font-inter text-sm font-medium text-gray-700 hover:text-gray-500'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className='font-inter text-sm font-medium text-gray-700 hover:text-gray-500'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  className='mt-5 flex w-full items-center justify-center rounded-full border border-black bg-black px-5 py-1.5 text-center font-inter text-sm text-white transition-all hover:bg-white hover:text-black'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
